3376d58035631d6fab32e6c56d8cf8347d63a22e
daynadrant <dayna.drant@gmail.com>
2025-09-01 20:35:46 -0400

    init commit

---
+++ assets/vendor/php-email-form/validate.js
@@ -0,0 +1,89 @@
+/**
+* PHP Email Form Validation - v3.10
+* URL: https://bootstrapmade.com/php-email-form/
+* Author: BootstrapMade.com
+*/
+(function () {
+  "use strict";
+
+  let forms = document.querySelectorAll('.php-email-form');
+
+  forms.forEach(function (e) {
+    e.addEventListener('submit', function (event) {
+      event.preventDefault();
+
+      let thisForm = this;
+
+      let action = thisForm.getAttribute('action');
+      let recaptcha = thisForm.getAttribute('data-recaptcha-site-key');
+
+      if (!action) {
+        displayError(thisForm, 'The form action property is not set!');
+        return;
+      }
+      thisForm.querySelector('.loading').classList.add('d-block');
+      thisForm.querySelector('.error-message').classList.remove('d-block');
+      thisForm.querySelector('.sent-message').classList.remove('d-block');
+
+      let formData = new FormData(thisForm);
+
+      if (recaptcha) {
+        if (typeof grecaptcha !== "undefined") {
+          grecaptcha.ready(function () {
+            try {
+              grecaptcha.execute(recaptcha, { action: 'php_email_form_submit' })
+                .then(token => {
+                  formData.set('recaptcha-response', token);
+                  php_email_form_submit(thisForm, action, formData);
+                })
+            } catch (error) {
+              displayError(thisForm, error);
+            }
+          });
+        } else {
+          displayError(thisForm, 'The reCaptcha javascript API url is not loaded!')
+        }
+      } else {
+        php_email_form_submit(thisForm, action, formData);
+      }
+    });
+  });
+
+  function php_email_form_submit(thisForm, action, formData) {
+    fetch(action, {
+      method: 'POST',
+      body: formData,
+      headers: { 'Accept': 'application/json' }
+    })
+      .then(response => {
+        if (response.ok) {
+          return response.json(); // Parse the response as JSON
+        } else {
+          throw new Error(`${response.status} ${response.statusText} ${response.url}`);
+        }
+      })
+      .then(data => {
+        thisForm.querySelector('.loading').classList.remove('d-block');
+        if (data.ok) { // Check the "ok" property in the JSON object
+          thisForm.querySelector('.sent-message').classList.add('d-block');
+          thisForm.reset();
+
+          // Optionally, you can hide the form and show the message
+          // thisForm.style.display = 'none';
+
+        } else {
+          throw new Error(data.error ? data.error : 'Form submission failed. Please try again.');
+        }
+      })
+      .catch((error) => {
+        displayError(thisForm, error);
+      });
+  }
+
+  function displayError(thisForm, error) {
+    thisForm.querySelector('.loading').classList.remove('d-block');
+    thisForm.querySelector('.error-message').innerHTML = error;
+    thisForm.querySelector('.error-message').classList.add('d-block');
+  }
+
+})();
